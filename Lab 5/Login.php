4ad0817229d3c29ff27351101d52b948bb3e38ad
Ahnaf Sayed <ahnafsayedtashri@gmail.com>
2021-06-20 17:18:08 +0600

    Mid project on process!

---
+++ Lab 5/Login.php
@@ -0,0 +1,33 @@
+<html>
+	<head></head>
+	<body>
+		<center>
+		<form action="" method="post">
+		<fieldset style="width:270px" "height:300px">
+		<legend align="center"><h1><b>Login</b></h1></legend>
+			<table>			
+				<tr>
+					<td><span >Username:</span></td>
+					<td></td>
+					<td><input type="text" placeholder="Type your Username!"</span></td>
+				</tr>
+				<tr>
+					<td><span>Password:</span></td>
+					<td></td>
+					<td><input type="password" placeholder="Type your Password!"</span></td>
+				</tr>
+				<tr>
+				<td colspan="3" align="center">
+				<input type="Submit" value="Login">
+				</td>
+				</tr>
+				<br>
+				<tr>
+				<td><h5>Don't have an account?</h5><a href="Signup.php"><h5>Sign Up!</h5></a></td>
+				</tr>
+			</table>
+	</fieldset>
+		</form>
+		</center>
+	</body>
+</html>
