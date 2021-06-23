3695be43624554bec1ad97c41a73db74992d142d
Ahnaf Sayed <ahnafsayedtashri@gmail.com>
2021-06-23 19:46:20 +0600

    Project on process!

--- Lab 5/Login.php
+++ Lab 5/Login.php
@@ -1,3 +1,73 @@
+<?php
+
+	$username="";
+	$err_username="";
+	
+	$password="";
+	$err_password="";
+	
+	if	($_SERVER["REQUEST_METHOD"]=="POST")
+		{
+          if (strlen($_POST["username"])<6)
+			{
+				$err_username="Username length must contain 6 characters!";
+			}
+			elseif(strpos($_POST["username"]," "))
+			{
+				$err_username="Space is not allowed!";
+			}
+			else
+			{
+				$username=$_POST["username"];
+			}
+			if(strlen($_POST["password"])>8)
+			{
+				$pass=$_POST["password"];
+			if ((!strpos($_POST["password"],"#"))||(!strpos($_POST["password"],"?")))
+				{
+				$err_password="Password must contain at least 8 characters and 1 special character!";
+				}
+				for ($i=0;$l<strlen($_POST["password"]);$i++)
+				{
+					if (ctype_lower($_POST["password"][$i]))
+					{
+						break;
+					}
+					else
+					{
+						$err_password="Password must contain minimum 1 lowercase alphabet!";
+					}
+				}
+				for ($j=0;$m<strlen($_POST["password"]);$j++)
+				{
+					if (ctype_upper($_POST["password"][$j]))
+					{
+						break;
+					}
+					else
+					{
+						$err_pass="Password must contain minimum 1 uppercase alphabet!";
+					}
+				}
+				for($k=0;$n<strlen($_POST["password"]);$k++)
+				{
+					if(is_numeric($_POST["password"][$k]))
+					{
+						break;
+					}
+					else
+					{
+						$err_password="Password must contain minimum 1 numeric character!";
+					}
+				}
+			}
+			else	
+			$err_password="Password length must be 8 or longer!";
+		}
+?>		
+
+
+
 <html>
 	<head></head>
 	<body>
@@ -9,12 +79,13 @@
 				<tr>
 					<td><span >Username:</span></td>
 					<td></td>
-					<td><input type="text" placeholder="Type your Username!"</span></td>
+					<td><input type="text" name="username" value="<?php echo $username;?>"> </td><td><span><?php echo $err_username;?></span></td>
 				</tr>
+				<br>
 				<tr>
 					<td><span>Password:</span></td>
 					<td></td>
-					<td><input type="password" placeholder="Type your Password!"</span></td>
+					<td><input type="password" name="password" value="<?php echo $password;?>"> </td><td><span><?php echo $err_password;?></span></td>
 				</tr>
 				<tr>
 				<td colspan="3" align="center">
@@ -26,7 +97,7 @@
 				<td><h5>Don't have an account?</h5><a href="Signup.php"><h5>Sign Up!</h5></a></td>
 				</tr>
 			</table>
-	</fieldset>
+	    </fieldset>
 		</form>
 		</center>
 	</body>
